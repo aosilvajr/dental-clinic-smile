95001951704c8abadaf280c3f85ad422c0ce2a82
Ailton Jr <aosilvajr@gmail.com>
2021-05-01 17:47:50 -0300

    test: add Login route

--- src/main/routes/login.routes.test.ts
+++ src/main/routes/login.routes.test.ts
@@ -1,11 +1,24 @@
+import { hash } from 'bcrypt'
 import faker from 'faker'
+import { Collection } from 'mongodb'
 import request from 'supertest'
 
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 
 import app from '../config/app'
 
+const fakePassword = faker.internet.password()
+
+const fakeAccount = {
+  name: faker.internet.userName(),
+  email: faker.internet.email(),
+  password: fakePassword,
+  passwordConfirmation: fakePassword
+}
+
 describe('Login Routes', () => {
+  let accountCollection: Collection
+
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -15,18 +28,42 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
+
   describe('POST /signup', () => {
     test('Should return 200 on signup', async () => {
       await request(app)
         .post('/api/signup')
+        .send(fakeAccount)
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      await accountCollection.insertOne({
+        name: 'aosilvajr',
+        email: 'aosilvajr@gmail.com',
+        password: await hash(fakeAccount.password, 12)
+      })
+
+      await request(app)
+        .post('/api/login')
         .send({
-          name: faker.internet.userName(),
-          email: faker.internet.email(),
-          password: 'any_value',
-          passwordConfirmation: 'any_value'
+          email: 'aosilvajr@gmail.com',
+          password: fakeAccount.password
+        })
+        .expect(200)
+    })
+
+    test('Should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'aosilvajr@gmail.com',
+          password: fakeAccount.password
         })
         .expect(200)
     })

08e85b54d874bcd596fb4888a3aec0cac162f573
Ailton Jr <aosilvajr@gmail.com>
2021-05-01 17:47:30 -0300

    feat: add Login route

--- src/main/routes/login.routes.ts
+++ src/main/routes/login.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 
 import { adaptRoute } from '../adapter/express/express-route-adapter'
+import { makeLoginController } from '../factories/login/login-factory'
 import { makeSignUpController } from '../factories/signup/signup-factory'
 
 export default (route: Router): void => {
   route.post('/signup', adaptRoute(makeSignUpController()))
+  route.post('/login', adaptRoute(makeLoginController()))
 }

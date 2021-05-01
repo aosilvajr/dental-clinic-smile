c418f27777ad454b764bd10441c8d5c6dadb7966
Ailton Jr <aosilvajr@gmail.com>
2021-05-01 15:33:12 -0300

    test: ensure BcryptAdapter returns false when compare fails

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -52,4 +52,15 @@ describe('Bcrypt Adapter', () => {
     const isValid = await sut.compare('any_value', 'any_isValid')
     expect(isValid).toBe(true)
   })
+
+  test('Should return a false when compare fails', async () => {
+    const sut = makeSut()
+
+    jest
+      .spyOn(bcrypt, 'compare')
+      .mockReturnValueOnce(Promise.resolve(false))
+
+    const isValid = await sut.compare('any_value', 'any_isValid')
+    expect(isValid).toBe(false)
+  })
 })

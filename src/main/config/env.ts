2ab80019b8cb31893299319411fa6c0cdd1fabcf
Ailton Jr <aosilvajr@gmail.com>
2021-05-01 17:17:45 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/clinic_smile',
-  PORT: process.env.PORT || 5050
+  PORT: process.env.PORT || 5050,
+  JWT_SECRET: '16fa2646865e29db963b87b23023c1f5'
 }

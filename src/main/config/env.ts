38ba1c26817b5a179c888eba42b66bf8cda00324
Ailton Jr <aosilvajr@gmail.com>
2021-05-02 12:29:58 -0300

    fix: mongodb url

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/clinic_smile',
+  MONGO_URL: process.env.MONGO_URL || 'mongodb+srv://aosilvajr:RkpwGu7f5fj3Ou1l@clusterapi.av9rp.mongodb.net/smile?retryWrites=true&w=majority',
   PORT: process.env.PORT || 5050,
   JWT_SECRET: '16fa2646865e29db963b87b23023c1f5'
 }

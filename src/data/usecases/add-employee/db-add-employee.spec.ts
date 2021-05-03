4ef3c2362d86c9ce95efffe19f5fef8ef32d321d
Ailton Jr <aosilvajr@gmail.com>
2021-05-03 10:55:43 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/add-employee/db-add-employee.spec.ts
+++ src/data/usecases/add-employee/db-add-employee.spec.ts
@@ -14,16 +14,35 @@ const fakeEmployeeData: AddEmployeeModel = {
   birthday: faker.date.past()
 }
 
+const makeAddEmployeeRepository = (): AddEmployeeRepository => {
+  class AddEmployeeRepositoryStub implements AddEmployeeRepository {
+    async add (employeeData: AddEmployeeModel): Promise<void> {
+      return Promise.resolve()
+    }
+  }
+
+  return new AddEmployeeRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbAddEmployee,
+  addEmployeeRepositoryStub: AddEmployeeRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addEmployeeRepositoryStub = makeAddEmployeeRepository()
+  const sut = new DbAddEmployee(addEmployeeRepositoryStub)
+
+  return {
+    sut,
+    addEmployeeRepositoryStub
+  }
+}
+
 describe('DbAddEmployee Usecase', () => {
   test('Should call AddEmployeeRepository with correct values', async () => {
-    class AddEmployeeRepositoryStub implements AddEmployeeRepository {
-      async add (employeeData: AddEmployeeModel): Promise<void> {
-        return Promise.resolve()
-      }
-    }
-    const addEmployeeRepositoryStub = new AddEmployeeRepositoryStub()
+    const { sut, addEmployeeRepositoryStub } = makeSut()
     const addSpy = jest.spyOn(addEmployeeRepositoryStub, 'add')
-    const sut = new DbAddEmployee(addEmployeeRepositoryStub)
     const employeeData = fakeEmployeeData
     await sut.add(employeeData)
     expect(addSpy).toHaveBeenCalledWith(employeeData)

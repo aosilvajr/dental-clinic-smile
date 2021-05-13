ed874bec5f8853bca0d9c357e575cb968b9923c5
Ailton Jr <aosilvajr@gmail.com>
2021-05-12 22:02:47 -0300

    feat: ensure EmployeeMongoRepository load all employees

--- src/infra/db/mongodb/employee/employee-mongo-repository.ts
+++ src/infra/db/mongodb/employee/employee-mongo-repository.ts
@@ -1,11 +1,19 @@
+import { LoadEmployeesRepository } from '@/data/protocols/db/account/load-employees-repository'
 import { AddEmployeeRepository } from '@/data/usecases/add-employee/db-add-employee-protocols'
+import { EmployeeModel } from '@/domain/models/employee'
 import { AddEmployeeModel } from '@/domain/usecases/add-employee'
 
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class EmployeeMongoRepository implements AddEmployeeRepository {
+export class EmployeeMongoRepository implements AddEmployeeRepository, LoadEmployeesRepository {
   async add (employeeData: AddEmployeeModel): Promise<void> {
     const employeeCollection = await MongoHelper.getCollection('employees')
     await employeeCollection.insertOne(employeeData)
+  }
+
+  async loadAll (): Promise<EmployeeModel[]> {
+    const employeeCollection = await MongoHelper.getCollection('employees')
+    const employees: EmployeeModel[] = await employeeCollection.find().toArray()
+    return employees
   }
 }

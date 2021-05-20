8283e26a41e095e360f52fc62dda0df104d8d3b9
Ailton Jr <aosilvajr@gmail.com>
2021-05-20 12:39:10 -0300

    feat: ensure EmployeeMongoRepository find employee by id with success

--- src/infra/db/mongodb/employee/employee-mongo-repository.ts
+++ src/infra/db/mongodb/employee/employee-mongo-repository.ts
@@ -1,11 +1,12 @@
 import { LoadEmployeesRepository } from '@/data/protocols/db/account/load-employees-repository'
 import { AddEmployeeRepository } from '@/data/usecases/add-employee/db-add-employee-protocols'
+import { LoadEmployeeByIdRepository } from '@/data/usecases/load-employee-by-id/db-load-employee-by-id-protocols'
 import { EmployeeModel } from '@/domain/models/employee'
 import { AddEmployeeModel } from '@/domain/usecases/add-employee'
 
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class EmployeeMongoRepository implements AddEmployeeRepository, LoadEmployeesRepository {
+export class EmployeeMongoRepository implements AddEmployeeRepository, LoadEmployeesRepository, LoadEmployeeByIdRepository {
   async add (employeeData: AddEmployeeModel): Promise<void> {
     const employeeCollection = await MongoHelper.getCollection('employees')
     await employeeCollection.insertOne(employeeData)
@@ -13,7 +14,13 @@ export class EmployeeMongoRepository implements AddEmployeeRepository, LoadEmplo
 
   async loadAll (): Promise<EmployeeModel[]> {
     const employeeCollection = await MongoHelper.getCollection('employees')
-    const employees: EmployeeModel[] = await employeeCollection.find().toArray()
+    const employees = await employeeCollection.find().toArray()
     return employees
+  }
+
+  async loadById (id: string): Promise<EmployeeModel> {
+    const employeeCollection = await MongoHelper.getCollection('employees')
+    const employee = await employeeCollection.findOne({ _id: id })
+    return employee
   }
 }

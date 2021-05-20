117476d9a3fc3c62832ddda5186b569d7758e441
Ailton Jr <aosilvajr@gmail.com>
2021-05-20 11:11:24 -0300

    feat: add patient model

---
+++ src/domain/models/patient.ts
@@ -0,0 +1,13 @@
+export type PatientModel = {
+  id: string
+  name: string
+  email: string
+  phone: string
+  whatsapp: string
+  occupation: string
+  indication: string
+  acceptMessage: string
+  startTreatment: Date
+  endTreatment: Date
+  createdAt: Date
+}

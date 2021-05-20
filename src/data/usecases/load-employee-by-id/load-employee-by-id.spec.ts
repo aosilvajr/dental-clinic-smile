8a2a8a4200933e660fc21e93e404e35ca9c90adf
Ailton Jr <aosilvajr@gmail.com>
2021-05-20 11:48:16 -0300

    test: ensure DbLoadEmployeeById throws if LoadEmployeeByIdRepository throws

--- src/data/usecases/load-employee-by-id/load-employee-by-id.spec.ts
+++ src/data/usecases/load-employee-by-id/load-employee-by-id.spec.ts
@@ -53,13 +53,24 @@ describe('DbLoadEmployeeById', () => {
   test('Should call LoadEmployeeByIdRepository', async () => {
     const { sut, loadEmployeeByIdRepositoryStub } = makeSut()
     const loadByIdSpy = jest.spyOn(loadEmployeeByIdRepositoryStub, 'loadById')
-    await sut.loadById('any_id')
-    expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+    await sut.loadById(makeFakeEmployee.id)
+    expect(loadByIdSpy).toHaveBeenCalledWith(makeFakeEmployee.id)
   })
 
   test('Should return employee on success', async () => {
     const { sut } = makeSut()
-    const employee = await sut.loadById('any_id')
+    const employee = await sut.loadById(makeFakeEmployee.id)
     expect(employee).toEqual(makeFakeEmployee)
+  })
+
+  test('Should throw if LoadEmployeeByIdRepository throws', async () => {
+    const { sut, loadEmployeeByIdRepositoryStub } = makeSut()
+
+    jest
+      .spyOn(loadEmployeeByIdRepositoryStub, 'loadById')
+      .mockReturnValueOnce(Promise.reject(new Error()))
+
+    const promise = sut.loadById(makeFakeEmployee.id)
+    await expect(promise).rejects.toThrow()
   })
 })

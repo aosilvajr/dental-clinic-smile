838864c91b1e1af0466ed2a277dbca8fed1b35bf
Ailton Jr <aosilvajr@gmail.com>
2021-05-21 12:56:46 -0300

    test: ensure LoadEmployeeByIdController returns 500 if LoadEmployeById throws

--- src/presentation/controllers/employee/load-employee-by-id/load-employee-by-id-controller.spec.ts
+++ src/presentation/controllers/employee/load-employee-by-id/load-employee-by-id-controller.spec.ts
@@ -1,7 +1,7 @@
 import faker from 'faker'
 
 import { InvalidParamError } from '@/presentation/errors'
-import { forbidden } from '@/presentation/helper/http/http-helper'
+import { forbidden, serverError } from '@/presentation/helper/http/http-helper'
 
 import { LoadEmployeeByIdController } from './load-employee-by-id-controller'
 import { httpRequest, LoadEmployeeById, EmployeeModel } from './load-employee-by-id-controller-protocols'
@@ -63,5 +63,16 @@ describe('LoadEmployeeById Controller', () => {
 
     const httpResponse = await sut.handle(makeFakeRequest)
     expect(httpResponse).toEqual(forbidden(new InvalidParamError('employeeId')))
+  })
+
+  test('Should return 500 if LoadEmployeById throws', async () => {
+    const { sut, loadEmployeeByIdStub } = makeSut()
+
+    jest
+      .spyOn(loadEmployeeByIdStub, 'loadById')
+      .mockReturnValueOnce(Promise.reject(new Error()))
+
+    const httpResponse = await sut.handle(makeFakeRequest)
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })

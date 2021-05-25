edf4e796f784ab18faadf0b5d11a1ca28257d332
Ailton Jr <aosilvajr@gmail.com>
2021-05-25 13:05:54 -0300

    test: ensure DeleteEmployeeController return 204 if DeleteEmployee succeeds

--- src/presentation/controllers/employee/delete-employee/delete-employee.spec.ts
+++ src/presentation/controllers/employee/delete-employee/delete-employee.spec.ts
@@ -2,13 +2,13 @@ import faker from 'faker'
 
 import { throwError } from '@/domain/test'
 import { DeleteEmployee } from '@/domain/usecases/employee/delete-employee'
-import { serverError } from '@/presentation/helper/http/http-helper'
+import { noContent, serverError } from '@/presentation/helper/http/http-helper'
 import { httpRequest } from '@/presentation/protocols'
 import { mockDeleteEmployee } from '@/presentation/test'
 
 import { DeleteEmployeeController } from './delete-employee'
 
-const makeFakeRequest: httpRequest = {
+const mockRequest: httpRequest = {
   params: {
     employeeId: faker.datatype.uuid()
   }
@@ -33,8 +33,8 @@ describe('DeleteEmployee Controller', () => {
   test('Should call DeleteEmployee with correct values', async () => {
     const { sut, deleteEmployeeStub } = makeSut()
     const deleteSpy = jest.spyOn(deleteEmployeeStub, 'delete')
-    await sut.handle(makeFakeRequest)
-    expect(deleteSpy).toHaveBeenCalledWith(makeFakeRequest.params.employeeId)
+    await sut.handle(mockRequest)
+    expect(deleteSpy).toHaveBeenCalledWith(mockRequest.params.employeeId)
   })
 
   test('Should return 500 if DeleteEmployee throws', async () => {
@@ -44,7 +44,13 @@ describe('DeleteEmployee Controller', () => {
       .spyOn(deleteEmployeeStub, 'delete')
       .mockImplementationOnce(throwError)
 
-    const httpResponse = await sut.handle(makeFakeRequest)
+    const httpResponse = await sut.handle(mockRequest)
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(mockRequest)
+    expect(httpResponse).toEqual(noContent())
   })
 })

5e5885aa98e1daffb4819a51fc08b4292f2003df
Ailton Jr <aosilvajr@gmail.com>
2021-06-03 11:40:49 -0300

    refactor: move sut creation to factory method

--- src/data/usecases/patient/add-patient/add-patient.spec.ts
+++ src/data/usecases/patient/add-patient/add-patient.spec.ts
@@ -1,12 +1,27 @@
+import { AddPatientRepository } from '@/data/protocols/db/patient/add-patient-repository'
 import { AddPatientRepositoryStub } from '@/data/test'
 import { mockPatientParams } from '@/domain/test'
 
 import { DbAddPatient } from './add-patient'
 
+type SutTypes = {
+  sut: DbAddPatient,
+  addPatientRepositoryStub: AddPatientRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addPatientRepositoryStub = new AddPatientRepositoryStub()
+  const sut = new DbAddPatient(addPatientRepositoryStub)
+
+  return {
+    sut,
+    addPatientRepositoryStub
+  }
+}
+
 describe('DbAddPatient Usecase', () => {
   test('Should call AddPatientRepository with correct values', async () => {
-    const addPatientRepositoryStub = new AddPatientRepositoryStub()
-    const sut = new DbAddPatient(addPatientRepositoryStub)
+    const { sut, addPatientRepositoryStub } = makeSut()
     const addSpy = jest.spyOn(addPatientRepositoryStub, 'add')
     await sut.add(mockPatientParams)
     expect(addSpy).toHaveBeenCalledWith(mockPatientParams)

77b92176eabc98e27acdba253ca78dbc19e74593
Ailton Jr <aosilvajr@gmail.com>
2021-06-03 15:19:45 -0300

    test: ensure add patient route returns 204 if valid token provided

--- src/main/routes/patient.routes.test.ts
+++ src/main/routes/patient.routes.test.ts
@@ -1,12 +1,37 @@
+import faker from 'faker'
+import jwt from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 
+import { mockPatientParams } from '@/domain/test'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 
 import app from '../config/app'
+import env from '../config/env'
 
 let accountCollection: Collection
 let patientCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    role: 'admin'
+  })
+  const id = res.ops[0]._id
+  const accessToken = jwt.sign({ id }, env.JWT_SECRET)
+
+  await accountCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+
+  return accessToken
+}
 
 describe('Patient Routes', () => {
   beforeAll(async () => {
@@ -29,8 +54,18 @@ describe('Patient Routes', () => {
     test('Should return 403 on add patient without access token', async () => {
       await request(app)
         .post('/api/patients')
-        .send()
+        .send(mockPatientParams)
         .expect(403)
+    })
+
+    test('Should return 204 on add patient with valid token', async () => {
+      const accessToken = await makeAccessToken()
+
+      await request(app)
+        .post('/api/patients')
+        .set('x-access-token', accessToken)
+        .send(mockPatientParams)
+        .expect(204)
     })
   })
 })

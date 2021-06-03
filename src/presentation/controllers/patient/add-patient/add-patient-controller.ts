db6663d596e21ecf8b6dc52ae9854ecdff143454
Ailton Jr <aosilvajr@gmail.com>
2021-06-03 12:58:28 -0300

    feat: ensure AddPatientController calls AddPatient with correct values

--- src/presentation/controllers/patient/add-patient/add-patient-controller.ts
+++ src/presentation/controllers/patient/add-patient/add-patient-controller.ts
@@ -1,9 +1,11 @@
+import { AddPatient } from '@/domain/usecases/patient/add-patient'
 import { badRequest } from '@/presentation/helper/http/http-helper'
 import { Controller, httpRequest, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class AddPatientController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly addPatient: AddPatient
   ) { }
 
   async handle (httpRequest: httpRequest): Promise<HttpResponse> {
@@ -11,6 +13,7 @@ export class AddPatientController implements Controller {
     if (error) {
       return badRequest(error)
     }
+    await this.addPatient.add(httpRequest.body)
     return null
   }
 }
